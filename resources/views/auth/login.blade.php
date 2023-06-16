83a17e132047be1ed81834f5ca1e6ac2dc7c2954
negi84 <vipin@mindrops.com>
2023-06-16 12:54:16 +0530

    login template matched

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,85 +1,82 @@
-@extends('layouts.app', [
-    'class' => 'login-page',
-    'backgroundImagePath' => 'img/bg/fabio-mangione.jpg'
-])
+@extends('layouts/blankLayout')
 
-@section('content')
-    <div class="content">
-        <div class="container">
-            <div class="col-lg-4 col-md-6 ml-auto mr-auto">
-                <form class="form" method="POST" action="{{ route('login') }}">
-                    @csrf
-                    <div class="card card-login">
-                        <div class="card-header ">
-                            <div class="card-header ">
-                                <h3 class="header text-center">{{ __('Login') }}</h3>
-                            </div>
-                        </div>
-                        <div class="card-body ">
+@section('title', 'Login Basic - Pages')
 
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text">
-                                        <i class="nc-icon nc-single-02"></i>
-                                    </span>
-                                </div>
-                                <input class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{ __('Email') }}" type="email" name="email" value="{{ old('email') }}" required autofocus>
-                                
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" style="display: block;" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text">
-                                        <i class="nc-icon nc-single-02"></i>
-                                    </span>
-                                </div>
-                                <input class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="{{ __('Password') }}" type="password" required>
-                                
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" style="display: block;" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-
-                            <div class="form-group">
-                                <div class="form-check">
-                                     <label class="form-check-label">
-                                        <input class="form-check-input" name="remember" type="checkbox" value="" {{ old('remember') ? 'checked' : '' }}>
-                                        <span class="form-check-sign"></span>
-                                        {{ __('Remember me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="card-footer">
-                            <div class="text-center">
-                                <button type="submit" class="btn btn-warning btn-round mb-3">{{ __('Sign in') }}</button>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-                <a href="{{ route('password.request') }}" class="btn btn-link">
-                    {{ __('Forgot password') }}
-                </a>
-                <a href="{{ route('register') }}" class="btn btn-link float-right">
-                    {{ __('Create Account') }}
-                </a>
-            </div>
-        </div>
-    </div>
+@section('page-style')
+<!-- Page -->
+<link rel="stylesheet" href="{{asset('assets/vendor/css/pages/page-auth.css')}}">
 @endsection
 
-@push('scripts')
-    <script>
-        $(document).ready(function() {
-            demo.checkFullPageBackgroundImage();
-        });
-    </script>
-@endpush
+@section('content')
+<div class="container-xxl">
+  <div class="authentication-wrapper authentication-basic container-p-y">
+    <div class="authentication-inner">
+      <!-- Register -->
+      <div class="card">
+        <div class="card-body">
+          <!-- Logo -->
+          <div class="app-brand justify-content-center">
+            <a href="{{url('/')}}" class="app-brand-link gap-2">
+              <span class="app-brand-logo demo">@include('_partials.macros',["width"=>25,"withbg"=>'#696cff'])</span>
+              <span class="app-brand-text demo text-body fw-bolder">{{config('variables.templateName')}}</span>
+            </a>
+          </div>
+          <!-- /Logo -->
+          <h4 class="mb-2">Welcome to {{config('variables.templateName')}}! 👋</h4>
+          <p class="mb-4">Please sign-in to your account and start the adventure</p>
+
+          <form id="formAuthentication" class="form" class="mb-3" action="{{route('login')}}" method="POST">
+            @csrf
+            <div class="mb-3">
+              <label for="email" class="form-label">Email</label>
+              <input type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"  id="email" name="email" placeholder="Enter your email" value="{{ old('email') }}" required autofocus>
+              @if ($errors->has('email'))
+                <span class="invalid-feedback" style="display: block;" role="alert">
+                    <strong>{{ $errors->first('email') }}</strong>
+                </span>
+             @endif
+            </div>
+            <div class="mb-3 form-password-toggle">
+              <div class="d-flex justify-content-between">
+                <label class="form-label" for="password">Password</label>
+                <a href="{{ route('password.request') }}">
+                  <small>Forgot Password?</small>
+                </a>
+              </div>
+              <div class="input-group input-group-merge">
+                <input type="password" id="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="password"  required/>
+                <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                @if ($errors->has('password'))
+                    <span class="invalid-feedback" style="display: block;" role="alert">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                @endif
+              </div>
+            </div>
+            <div class="mb-3">
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="remember-me" value="" {{ old('remember') ? 'checked' : '' }}>
+                <label class="form-check-label" for="remember-me">
+                  Remember Me
+                </label>
+              </div>
+            </div>
+            <div class="mb-3">
+              <button class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
+            </div>
+          </form>
+
+          <p class="text-center">
+            <span>New on our platform?</span>
+            <a href="{{ route('register') }}">
+              <span>Create an account</span>
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+    <!-- /Register -->
+  </div>
+</div>
+</div>
+@endsection
