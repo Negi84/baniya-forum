eca5aeef3228cfac16a1c7e781b76c30f580bcb1
negi84 <vipin@mindrops.com>
2023-06-16 13:11:34 +0530

    registeration done

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,125 +1,94 @@
-@extends('layouts.app', [
-    'class' => 'register-page',
-    'backgroundImagePath' => 'img/bg/jan-sendereks.jpg'
-])
+@extends('layouts/blankLayout')
+
+@section('title', 'Register Basic - Pages')
+
+@section('page-style')
+    <!-- Page -->
+    <link rel="stylesheet" href="{{ asset('assets/vendor/css/pages/page-auth.css') }}">
+@endsection
+
 
 @section('content')
-    <div class="content">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-5 col-md-5 ml-auto">
-                    <div class="info-area info-horizontal mt-5">
-                        <div class="icon icon-primary">
-                            <i class="nc-icon nc-tv-2"></i>
+    <div class="container-xxl">
+        <div class="authentication-wrapper authentication-basic container-p-y">
+            <div class="authentication-inner">
+
+                <!-- Register Card -->
+                <div class="card">
+                    <div class="card-body">
+                        <!-- Logo -->
+                        <div class="app-brand justify-content-center">
+                            <a href="{{ url('/') }}" class="app-brand-link gap-2">
+                                <span class="app-brand-logo demo">@include('_partials.macros', ['width' => 25, 'withbg' => '#696cff'])</span>
+                                <span
+                                    class="app-brand-text demo text-body fw-bolder">{{ config('variables.templateName') }}</span>
+                            </a>
                         </div>
-                        <div class="description">
-                            <h5 class="info-title">{{ __('Marketing') }}</h5>
-                            <p class="description">
-                                {{ __('We\'ve created the marketing campaign of the website. It was a very interesting collaboration.') }}
-                            </p>
-                        </div>
-                    </div>
-                    <div class="info-area info-horizontal">
-                        <div class="icon icon-primary">
-                            <i class="nc-icon nc-html5"></i>
-                        </div>
-                        <div class="description">
-                            <h5 class="info-title">{{ __('Fully Coded in HTML5') }}</h5>
-                            <p class="description">
-                                {{ __('We\'ve developed the website with HTML5 and CSS3. The client has access to the code using GitHub.') }}
-                            </p>
-                        </div>
-                    </div>
-                    <div class="info-area info-horizontal">
-                        <div class="icon icon-info">
-                            <i class="nc-icon nc-atom"></i>
-                        </div>
-                        <div class="description">
-                            <h5 class="info-title">{{ __('Built Audience') }}</h5>
-                            <p class="description">
-                                {{ __('There is also a Fully Customizable CMS Admin Dashboard for this product.') }}
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 mr-auto">
-                    <div class="card card-signup text-center">
-                        <div class="card-header ">
-                            <h4 class="card-title">{{ __('Register') }}</h4>
-                            <div class="social">
-                                <button class="btn btn-icon btn-round btn-twitter">
-                                    <i class="fa fa-twitter"></i>
-                                </button>
-                                <button class="btn btn-icon btn-round btn-dribbble">
-                                    <i class="fa fa-dribbble"></i>
-                                </button>
-                                <button class="btn btn-icon btn-round btn-facebook">
-                                    <i class="fa fa-facebook-f"></i>
-                                </button>
-                                <p class="card-description">{{ __('or be classical') }}</p>
+                        <!-- /Logo -->
+                        <h4 class="mb-2">Adventure starts here 🚀</h4>
+                        <p class="mb-4">Make your app management easy and fun!</p>
+
+                        <form id="formAuthentication" class="mb-3" action="{{ route('register') }}" method="POST">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="username"
+                                    class="form-label {{ $errors->has('name') ? ' has-danger' : '' }}">Username</label>
+                                <input type="text" class="form-control" name="name" id="username"
+                                    value="{{ old('name') }}" required autofocus>
+                                @if ($errors->has('name'))
+                                    <span class="invalid-feedback" style="display: block;" role="alert">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
                             </div>
-                        </div>
-                        <div class="card-body ">
-                            <form class="form" method="POST" action="{{ route('register') }}">
-                                @csrf
-                                <div class="input-group{{ $errors->has('name') ? ' has-danger' : '' }}">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text">
-                                            <i class="nc-icon nc-single-02"></i>
-                                        </span>
-                                    </div>
-                                    <input name="name" type="text" class="form-control" placeholder="Name" value="{{ old('name') }}" required autofocus>
-                                    @if ($errors->has('name'))
-                                        <span class="invalid-feedback" style="display: block;" role="alert">
-                                            <strong>{{ $errors->first('name') }}</strong>
-                                        </span>
-                                    @endif
-                                </div>
-                                <div class="input-group{{ $errors->has('email') ? ' has-danger' : '' }}">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text">
-                                            <i class="nc-icon nc-email-85"></i>
-                                        </span>
-                                    </div>
-                                    <input name="email" type="email" class="form-control" placeholder="Email" required value="{{ old('email') }}">
-                                    @if ($errors->has('email'))
-                                        <span class="invalid-feedback" style="display: block;" role="alert">
-                                            <strong>{{ $errors->first('email') }}</strong>
-                                        </span>
-                                    @endif
-                                </div>
-                                <div class="input-group{{ $errors->has('password') ? ' has-danger' : '' }}">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text">
-                                            <i class="nc-icon nc-key-25"></i>
-                                        </span>
-                                    </div>
-                                    <input name="password" type="password" class="form-control" placeholder="Password" required>
+                            <div class="mb-3">
+                                <label for="email"
+                                    class="form-label {{ $errors->has('email') ? ' has-danger' : '' }}">Email</label>
+                                <input type="email" class="form-control" id="email" name="email" placeholder="Email"
+                                    required value="{{ old('email') }}">
+                                @if ($errors->has('email'))
+                                    <span class="invalid-feedback" style="display: block;" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                            <div class="mb-3 form-password-toggle">
+                                <label class="form-label {{ $errors->has('password') ? ' has-danger' : '' }}"
+                                    for="password">Password</label>
+                                <div class="input-group input-group-merge">
+                                    <input name="password" type="password" class="form-control" placeholder="Password"
+                                        required aria-describedby="password" />
+                                    <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
                                     @if ($errors->has('password'))
                                         <span class="invalid-feedback" style="display: block;" role="alert">
                                             <strong>{{ $errors->first('password') }}</strong>
                                         </span>
                                     @endif
                                 </div>
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text">
-                                            <i class="nc-icon nc-key-25"></i>
-                                        </span>
-                                    </div>
-                                    <input name="password_confirmation" type="password" class="form-control" placeholder="Password confirmation" required>
+                            </div>
+
+                            <div class="mb-3 form-password-toggle">
+                                <label class="form-label {{ $errors->has('password') ? ' has-danger' : '' }}"
+                                    for="password">Password</label>
+                                <div class="input-group input-group-merge">
+                                    <input name="password_confirmation" type="password" class="form-control"
+                                        placeholder="Password confirmation" required />
+                                    <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
                                     @if ($errors->has('password_confirmation'))
                                         <span class="invalid-feedback" style="display: block;" role="alert">
                                             <strong>{{ $errors->first('password_confirmation') }}</strong>
                                         </span>
                                     @endif
                                 </div>
-                                <div class="form-check text-left">
-                                    <label class="form-check-label">
-                                        <input class="form-check-input" name="agree_terms_and_conditions" type="checkbox">
-                                        <span class="form-check-sign"></span>
-                                            {{ __('I agree to the') }}
-                                        <a href="#something">{{ __('terms and conditions') }}</a>.
+                            </div>
+
+                            <div class="mb-3">
+                                <div class="form-check">
+                                    <input class="form-check-input" name="agree_terms_and_conditions" type="checkbox"
+                                        id="terms-conditions" name="terms">
+                                    <label class="form-check-label" for="terms-conditions">
+                                        I agree to
+                                        <a href="javascript:void(0);">privacy policy & terms</a>
                                     </label>
                                     @if ($errors->has('agree_terms_and_conditions'))
                                         <span class="invalid-feedback" style="display: block;" role="alert">
@@ -127,22 +96,23 @@
                                         </span>
                                     @endif
                                 </div>
-                                <div class="card-footer ">
-                                    <button type="submit" class="btn btn-info btn-round">{{ __('Get Started') }}</button>
-                                </div>
-                            </form>
-                        </div>
+                            </div>
+                            <button class="btn btn-primary d-grid w-100">
+                                Sign up
+                            </button>
+                        </form>
+
+                        <p class="text-center">
+                            <span>Already have an account?</span>
+                            <a href="{{ route('login') }}">
+                                <span>Sign in instead</span>
+                            </a>
+                        </p>
                     </div>
                 </div>
-             </div>
+            </div>
+            <!-- Register Card -->
         </div>
-     </div> 
+    </div>
+    </div>
 @endsection
-
-@push('scripts')
-    <script>
-        $(document).ready(function() {
-            demo.checkFullPageBackgroundImage();
-        });
-    </script>
-@endpush
